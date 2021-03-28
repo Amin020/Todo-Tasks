155c445d104c597f2f0b518b7245e6d6da50258c
Mohamed Amin <m.amen@lagorta.com>
2021-03-28 11:49:09 +0200

    Removing unused component

--- src/app/modules/to-do/to-do.module.ts
+++ src/app/modules/to-do/to-do.module.ts
@@ -3,10 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ToDoComponent } from './to-do.component';
 import { MaterialModule } from 'src/app/core/material.module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { WarningDialogComponent } from './component/warning-dialog/warning-dialog.component';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { FormsModule } from '@angular/forms';
-import { AddNewTaskComponent } from './component/add-new-task/add-new-task.component';
 import { MyFilterPipe } from '../shared/pipes/filter-todos';
 
 @NgModule({
@@ -20,12 +18,9 @@ import { MyFilterPipe } from '../shared/pipes/filter-todos';
   ],
   declarations: [
     ToDoComponent,
-    WarningDialogComponent,
-    AddNewTaskComponent,
     MyFilterPipe
   ],
   entryComponents: [
-    AddNewTaskComponent
   ]
 })
 export class ToDoModule { }
